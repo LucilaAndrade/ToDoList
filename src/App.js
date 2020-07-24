b175b758e099ae76420e515e395f01f60afcf5dc
Lucila <lucila@lanube.mx>
2020-07-24 14:47:51 -0700

    ArreglosSimples

--- src/App.js
+++ src/App.js
@@ -1,67 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
+import task from "./sample/task.json";
 
-// function HelloWorld(props) {
-//   return (
-//     <div id="hello">
-//       <h3>{props.subtitle}</h3>
-//       {props.myText}
-//     </div>
-//   );
-// }
-
-class HelloWorld extends React.Component {
+class App extends Component {
   state = {
-    show: true,
-  };
-  toggleShow = () => {
-    //alert("welcome");
-    this.setState({ show: !this.state.show });
+    task: task,
   };
   render() {
-    if (this.state.show) {
-      return (
-        <div id="hello">
-          <h3>{this.props.subtitle}</h3>
-          {this.props.myText}
-          <button onClick={this.toggleShow}>Toogle show</button>
-        </div>
-      );
-    } else {
-      return (
-        <h1>
-          No hay elementos
-          <button onClick={this.toggleShow}>Toogle show</button>
-        </h1>
-      );
-    }
+    return (
+      <div>
+        {this.state.task.map((e) => (
+          <p key={e.id}>
+            {e.title} - {e.description} - {e.id}
+          </p>
+        ))}
+      </div>
+    );
   }
 }
-
-function App() {
-  return (
-    <div>
-      This is my component:
-      <HelloWorld myText="El primero" subtitle="Primer subtitulo" />
-      <HelloWorld myText="El segundo" subtitle="Segundo subtitulo" />
-      <HelloWorld myText="El tercero" subtitle="Tercer subtitulo" />
-    </div>
-  );
-}
-// const App1 = () => (
-//   <div>
-//     This is my component: <HelloWorld />
-//   </div>
-// );
-
-// class App2 extends React.Component {
-//   render() {
-//     return (
-//       <div>
-//         This is my component: <HelloWorld />
-//       </div>
-//     );
-//   }
-// }
-
 export default App;
