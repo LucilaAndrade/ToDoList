c3f60af7981d997a8b47385842e229f9beece775
Lucila <lucila@lanube.mx>
2020-07-24 14:23:14 -0700

    Ejercicio1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+// function HelloWorld(props) {
+//   return (
+//     <div id="hello">
+//       <h3>{props.subtitle}</h3>
+//       {props.myText}
+//     </div>
+//   );
+// }
+
+class HelloWorld extends React.Component {
+  state = {
+    show: true,
+  };
+  toggleShow = () => {
+    //alert("welcome");
+    this.setState({ show: !this.state.show });
+  };
+  render() {
+    if (this.state.show) {
+      return (
+        <div id="hello">
+          <h3>{this.props.subtitle}</h3>
+          {this.props.myText}
+          <button onClick={this.toggleShow}>Toogle show</button>
+        </div>
+      );
+    } else {
+      return (
+        <h1>
+          No hay elementos
+          <button onClick={this.toggleShow}>Toogle show</button>
+        </h1>
+      );
+    }
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      This is my component:
+      <HelloWorld myText="El primero" subtitle="Primer subtitulo" />
+      <HelloWorld myText="El segundo" subtitle="Segundo subtitulo" />
+      <HelloWorld myText="El tercero" subtitle="Tercer subtitulo" />
     </div>
   );
 }
+// const App1 = () => (
+//   <div>
+//     This is my component: <HelloWorld />
+//   </div>
+// );
+
+// class App2 extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         This is my component: <HelloWorld />
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
